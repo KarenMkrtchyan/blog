8a0f1923a5b4add271817b10902a72f14a93cd7c
KarenMkrtchyan <70455623+KarenMkrtchyan@users.noreply.github.com>
2024-01-02 16:35:50 -0800

    returns a json

--- tests/httpRequest.test.js
+++ tests/httpRequest.test.js
@@ -7,11 +7,21 @@ const api = supertest(app)
 beforeEach(async () => {
 	await Blog.deleteMany({})
 	await Blog.insertMany(helper.blogs)
+	jest.setTimeout(60000)
 })
 describe('http tests', () => {
-	test('get /api/blogs', async () => {
-		const response = await api.get('/api/blogs')
-		//console.log(response.body)
-		expect(response.body.length).toBe(helper.blogs.length)
+	test('return correct amont of notes in json format', async () => {
+		const response = await api
+			.get('/api/blogs')
+			.expect('Content-Type', /json/)
+			.expect(200)
+		expect(response.body.length).toEqual(helper.blogs.length)
+	})
+	test('.id property is defined instead of ._id', async () => {
+		let response = await api.get('/api/blogs')
+		response = response.body.map(r => r.id)
+		for (r of response) {
+			expect(r).toBeDefined()
+		}
 	})
 })
